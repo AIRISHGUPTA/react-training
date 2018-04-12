c200b722aab7bae182684aafd30611b77dc9f8fa
System Administrator <root@AIRISHs-Air.dev.evivehealth.com>
2018-04-12 12:46:18 +0530

    initial commit

--- src/actions/moviesActions.js
+++ src/actions/moviesActions.js
@@ -1,14 +1,14 @@
 import * as API from '../utils/api'
-export const STORE_MOVIES = "MOVIES/STORE_MOVIES"
-export const CHANGE_RATING = "MOVIES?CHANGE_RATING"
-export const fetchMoviesFromApi = () => (dispatch,getState) => {
+import * as types from '../constants/actionTypes'
+const fetchMoviesFromApi = () => (dispatch,getState) => {
   API.getMovies().then((movies)=>{dispatch(storeMovies(movies))});
 }
 
-export const storeMovies = (movies) => {
-  return {type:STORE_MOVIES,movies};
+const storeMovies = (movies) => {
+  return {type:types.STORE_MOVIES,movies};
 }
 
-export const changeRating = (id,rating) => {
-  return {type:CHANGE_RATING,id,rating}
+const changeRating = (id,rating) => {
+  return {type:types.CHANGE_RATING,id,rating}
 }
+export {fetchMoviesFromApi,storeMovies,changeRating};
--- src/reducers/moviesStoreReducer.js
+++ src/reducers/moviesStoreReducer.js
@@ -1,13 +1,13 @@
-import {STORE_MOVIES,CHANGE_RATING} from '../actions/moviesActions'
 
+import * as types from '../constants/actionTypes'
 export default function moviesStore(state={},action)
 {
   switch(action.type)
   {
-    case STORE_MOVIES:
+    case types.STORE_MOVIES:
       state = {movies:action.movies};
       break;
-    case CHANGE_RATING:
+    case types.CHANGE_RATING:
       //state = {...state,['movies']:{...['movies'],[action.id]:{rating:action.rating}}};
       state = {...state,['movies']:{
         ...state['movies'],
